d5b7c4ce36531a9d64f3a46324cf10b1f3ca2148
adiia11 <nadiaverbova007@gmail.com>
2026-06-13 10:23:58 +0300

    Add files via upload

---
+++ TrProgram.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Xml.Linq;
+namespace Transport2Exam
+{
+    /// <summary>
+    ///  Проєктування типів (Моделей даних):Пасажир характеризується числовим ідентифікатором (Id), прізвищем (LastName) 
+    ///  Passenger: Id,LastName, MaxLimSpent
+    ///  та індивідуальним добовим лімітом (максимально дозволеною сумою) витрат на транспорт 
+    ///  Маршрут (Транспорт) характеризується числовим ідентифікатором (Id), назвою маршруту (RouteTitle) 
+    ///  Passenger: Id,LastName, MaxLimSpent
+    /// Route: Id,Title(string), TicketPrice
+    ///   /// Infos: Date,PassengerId,RouteId, Count
+    ///  та базовою вартістю однієї поїздки
+    /// Infos: Date,PassengerId,RouteId, Count
+    ///  Журнал валідацій (Поїздок) містить дату та час поїздки (Date), ідентифікатор пасажира (PassengerId), ідентифікатор маршруту (RouteId) 
+    ///  та кількість пасажирів, за яких було оплачено одноразово цим пасажиром (Count).
+    ///  Інформацію про поїздки подано кількома (не менше 2) окремими XML-файлами,
+    ///  які в Main необхідно об'єднати через .Concat().2.
+    ///  Отримати та реалізувати:
+    ///  (а) Статистика завантаженості маршрутів:Отримати обʼєкт типу XElement,
+    ///  у якому для кожного маршруту вказати перелік пасажирів,  eachRoute allPassengers persSum+count (RtTitle,
+    ///  які ним користувалися, а також персональну сумарну кількість поїздок (з урахуванням сплачених додаткових пасажирів Count), 
+    ///  яку цей пасажир здійснив на цьому маршруті за весь період.Сортування: Перелік маршрутів впорядкувати за назвою/номером в 
+    ///  алфавітному порядку. 
+    ///  Перелік пасажирів всередині кожного маршруту впорядкувати за спаданням (від найбільшого до найменшого)
+    ///  сумарної кількості здійснених поїздок.Вимога до XML: Прізвища пацієнтів/пасажирів та їхню особисту кількість поїздок записати строго
+    ///  в XAttribute тегу <Passenger>. Отриманий результат вивести у файл TaskA.xml.(б) Звіт про дні перевищення ліміту витрат:Отримати обʼєкт 
+    ///  типу XElement, у якому для кожного пасажира вказати щодобові сумарні витрати на проїзд, але лише у ті дні, коли пасажир перевищив свій 
+    ///  особистий добовий ліміт витрат ($> \text{MaxExpenseLimit}$).Математика витрат за день: $\text{Кількість оплачених місць (Count)} \times
+    ///  \text{Вартість квитка на маршруті (TicketPrice)}$. Якщо пасажир за один день їздив кількома різними маршрутами, витрати за цей день додаються разом.
+    ///  Сортування: Перелік пасажирів впорядкувати за прізвищем у лексико-графічному порядку без повторень. 
+    ///  Дні всередині кожного пасажира впорядкувати за датами в хронологічному порядку 
+    ///  (від старіших до новіших).Вимога до XML: Дані про дату та сумарні витрачені кошти за цей день записати 
+    ///  строго в XAttribute вкладеного тегу <OverspentDay>. Отриманий результат вивести у файл TaskB.xml.
+    /// </summary>
+    public class TransportLogic {
+        //Отримати та реалізувати:
+        ///  (а) Статистика завантаженості маршрутів:Отримати обʼєкт типу XElement,
+        ///  у якому для кожного маршруту вказати перелік пасажирів,
+        ///  eachRoute allPassengers persSum+count (RtTitle,
+        ///  які ним користувалися, а також персональну сумарну кількість поїздок (з урахуванням сплачених додаткових пасажирів Count), 
+        ///  яку цей пасажир здійснив на цьому маршруті за весь період.
+        ///  Сортування: Перелік маршрутів впорядкувати за назвою/номером в 
+        ///  алфавітному порядку.  
+        ///  Перелік пасажирів всередині кожного маршруту впорядкувати за спаданням (від найбільшого до найменшого)
+        ///  сумарної кількості здійснених поїздок.Вимога до XML: Прізвища пацієнтів/пасажирів та їхню особисту кількість поїздок записати строго
+        ///  в XAttribute тегу <Passenger>
+        ///   Passenger: Id,LastName, MaxLimSpent
+        /// Route: Id,Title(string), TicketPrice
+        /// Infos: Date,PassengerId,RouteId, Count
+
+        public static XElement GetRouteStat(IEnumerable<XElement>passengers, IEnumerable<XElement> routes, IEnumerable<XElement> infos) {
+            var flatData = from i in infos
+                           join p in passengers on (int)i.Element("PassengerId")! equals (int)p.Element("Id")!
+                         //  where (string)p.Element("LastName")! == "Verbova"|| (string)p.Element("LastName")! == "Smith"
+                           join r in routes on (int)i.Element("RouteId")! equals (int)r.Element("Id")!
+                           select new
+                           {
+                               RouteTitle = (string)r.Element("Title")!,
+                               PasLastName = (string)p.Element("LastName")!,
+                               PasCount = (int)i.Element("Count")!,
+                          
+                           };
+            var eachRoute = from f in flatData
+                            group f by new { f.RouteTitle, f.PasLastName } into RouteGr
+                           
+                            let TotalPassCount = RouteGr.Sum(x => x.PasCount)
+
+                            select new
+                            {
+                                PassLastName = RouteGr.Key.PasLastName,
+                                RouteTitle = RouteGr.Key.RouteTitle,
+                                TotalPasCount = TotalPassCount
+
+                            };
+            var finalMerge = from e in eachRoute
+                             group e by e.RouteTitle into RouteGroup
+                             orderby RouteGroup.Key ascending
+                             select new XElement("Routes",
+                             new XElement("RouteTitle", RouteGroup.Key),
+                             from r in RouteGroup
+
+                             orderby r.TotalPasCount descending
+                             select new XElement("Passengers",
+                             new XElement("PassengerLastName", r.PassLastName),
+                             new XAttribute("TotalCount", r.TotalPasCount)
+                             ));
+
+            return new XElement("TravelStat", finalMerge);
+        }
+        ///  типу XElement, у якому для кожного пасажира вказати щодобові сумарні витрати на проїзд,
+        /// passLastName
+        /// але лише у ті дні, коли пасажир перевищив свій 
+        ///  особистий добовий ліміт витрат ($> \text{MaxExpenseLimit}$).Математика витрат за день: $\text{Кількість оплачених місць (Count)} \times
+        ///  \text{Вартість квитка на маршруті (TicketPrice)}$. Якщо пасажир за один день їздив кількома різними маршрутами, витрати за цей день додаються разом.
+        ///  Сортування: Перелік пасажирів впорядкувати за прізвищем у лексико-графічному порядку без повторень. 
+        ///  Дні всередині кожного пасажира впорядкувати за датами в хронологічному порядку 
+        ///  (від старіших до новіших).Вимога до XML: Дані про дату та сумарні витрачені кошти за цей день записати 
+        ///  строго в XAttribute вкладеного тегу <OverspentDay>. Отриманий результат вивести у файл TaskB.xml.
+        ///   Passenger: Id,LastName, MaxLimSpent
+        /// Route: Id,Title(string), TicketPrice
+        ///   /// Infos: Date,PassengerId,RouteId, Count
+        ///  passLastName where MaxLimSpent< DaySpent, Date
+        public static XElement OverLimitCommuting(IEnumerable<XElement> passengers, IEnumerable<XElement>routes, IEnumerable<XElement> infos)
+        {
+
+            var flatData = from i in infos
+                           join p in passengers on (int)i.Element("PassengerId")! equals (int)p.Element("Id")!
+                           join r in routes on (int)i.Element("RouteId")! equals (int)r.Element("Id")!
+                           select new
+                           {
+                               PassLastName = (string)p.Element("LastName")!,
+                               MaxLimSpent = (int)p.Element("MaxLimSpent")!,
+                               Date = (DateTime)i.Element("Date")!,
+                               AllTicketPrice=(int)r.Element("TicketPrice")! * (int)i.Element("Count")!,
+                               CountTic= (int)r.Element("TicketPrice")!
+
+                           };
+            var eachPass = from f in flatData
+                           group f by new { f.Date, f.PassLastName } into passGr
+                           let DaySpentM = passGr.Sum(x => x.AllTicketPrice)
+                           let pas=passGr.First()
+                           let ticketCount= passGr.Count()
+                           where DaySpentM>pas.MaxLimSpent
+                           select new
+                           {
+                               PasLastName = passGr.Key.PassLastName,
+                               DateTr = passGr.Key.Date,
+                               SpentMoney = DaySpentM,
+                               CountTick=ticketCount
+
+                              
+                           };
+            var finalMerge = from e in eachPass
+                             group e by e.PasLastName into PsGr
+                             orderby PsGr.Key ascending
+                             select new XElement("Passengers",
+                             new XElement("Passenger", PsGr.Key),
+                             from g in PsGr
+                             orderby g.DateTr ascending
+                             select new XElement("OverspentDay",
+                             new XAttribute("Date", g.DateTr.ToString("yyyy-MM-dd")),
+                             new XAttribute("DayTicketCount",g.CountTick),
+                             new XAttribute("SpentMoney", g.SpentMoney)
+
+                                 ));
+
+
+
+            return new XElement("OverLimStat", finalMerge);
+        }
+}
+
+    public class TrProgram
+    {
+        public static void Main(string[] args)
+        {
+
+            var passengers = XDocument.Load("passengers.xml").Descendants("Passenger");
+            var routes = XDocument.Load("routes.xml").Descendants("Route");
+            var info1 = XDocument.Load("info1.xml").Descendants("Info");
+            var info2 = XDocument.Load("info2.xml").Descendants("Info");
+            var infos = info1.Concat(info2);
+            string pathA = "D:\\Users\\LEGEND\\source\\repos\\Transport2Exam\\Transport2Exam\\TaskA.xml";
+            string pathB = "D:\\Users\\LEGEND\\source\\repos\\Transport2Exam\\Transport2Exam\\TaskB.xml";
+            var TaskA = TransportLogic.GetRouteStat(passengers, routes, infos);
+            var TaskB= TransportLogic.OverLimitCommuting(passengers, routes, infos);
+            TaskA.Save(pathA);
+            Console.WriteLine(TaskA.ToString());
+            TaskB.Save(pathB);
+            Console.WriteLine(TaskB.ToString());
+        }
+
+
+    }
+}
---
+++ TransportTests.cs
@@ -0,0 +1,176 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace TestProject1
+{
+	public class TransportFixture
+	{
+		public IEnumerable<XElement> Passengers { get; private set; }
+		public IEnumerable<XElement> Routes { get; private set; }
+		public IEnumerable<XElement> Info1 { get; private set; }
+		public IEnumerable<XElement> Info2 { get; private set; }
+		public IEnumerable<XElement> Infos { get; private set; }
+		public TransportFixture()
+		{
+			Passengers = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Passengers>
+	<Passenger>
+		<Id>1</Id>
+		<LastName>Berva</LastName>
+		<MaxLimSpent>50</MaxLimSpent>
+	</Passenger>
+	<Passenger>
+		<Id>2</Id>
+		<LastName>Aerbova</LastName>
+		<MaxLimSpent>40</MaxLimSpent>
+	</Passenger>
+<Passenger>
+	<Id>3</Id>
+	<LastName>Smith</LastName>
+	<MaxLimSpent>40</MaxLimSpent>
+</Passenger>
+</Passengers>").Descendants("Passenger");
+			Routes = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Routes>
+	<Route>
+		<Id>1</Id>
+		<Title>Troley</Title>
+		<TicketPrice>25</TicketPrice>
+	</Route>
+	<Route>
+		<Id>2</Id>
+		<Title>Bus</Title>
+		<TicketPrice>35</TicketPrice>
+	</Route>
+</Routes>").Descendants("Route");
+			Info1 = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Infos>
+	<Info>
+		<Date>2026-06-02</Date>
+		<PassengerId>1</PassengerId>
+		<RouteId>2</RouteId>
+		<Count>4</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-02</Date>
+		<PassengerId>2</PassengerId>
+		<RouteId>1</RouteId>
+		<Count>4</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-02</Date>
+		<PassengerId>2</PassengerId>
+		<RouteId>1</RouteId>
+		<Count>10</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-02</Date>
+		<PassengerId>3</PassengerId>
+		<RouteId>1</RouteId>
+		<Count>16</Count>
+	</Info>
+</Infos>").Descendants("Info");
+			Info2 = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<Infos>
+	<Info>
+		<Date>2026-06-06</Date>
+		<PassengerId>1</PassengerId>
+		<RouteId>1</RouteId>
+		<Count>2</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-06</Date>
+		<PassengerId>2</PassengerId>
+		<RouteId>1</RouteId>
+		<Count>4</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-06</Date>
+		<PassengerId>2</PassengerId>
+		<RouteId>2</RouteId>
+		<Count>10</Count>
+	</Info>
+	<Info>
+		<Date>2026-06-06</Date>
+		<PassengerId>3</PassengerId>
+		<RouteId>2</RouteId>
+		<Count>15</Count>
+	</Info>
+</Infos>").Descendants("Info");
+			Infos = Info1.Concat(Info2);
+
+
+		}
+	}
+
+		public class TransportTesting : IClassFixture<TransportFixture>
+		{
+		private readonly TransportFixture _fixture;
+		public TransportTesting(TransportFixture fixture)
+		{
+			_fixture = fixture;
+        }
+
+
+			[Fact]
+			public void Test1()
+			{
+			var exTree = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<TravelStat>
+  <Routes>
+    <RouteTitle>Bus</RouteTitle>
+    <Passengers TotalCount=""15"">
+      <PassengerLastName>Smith</PassengerLastName>
+    </Passengers>
+    <Passengers TotalCount=""10"">
+      <PassengerLastName>Aerbova</PassengerLastName>
+    </Passengers>
+    <Passengers TotalCount=""4"">
+      <PassengerLastName>Berva</PassengerLastName>
+    </Passengers>
+  </Routes>
+  <Routes>
+    <RouteTitle>Troley</RouteTitle>
+    <Passengers TotalCount=""18"">
+      <PassengerLastName>Aerbova</PassengerLastName>
+    </Passengers>
+    <Passengers TotalCount=""16"">
+      <PassengerLastName>Smith</PassengerLastName>
+    </Passengers>
+    <Passengers TotalCount=""2"">
+      <PassengerLastName>Berva</PassengerLastName>
+    </Passengers>
+  </Routes>
+</TravelStat>");
+			var acTree = Transport2Exam.TransportLogic.GetRouteStat(_fixture.Passengers, _fixture.Routes, _fixture.Infos);
+			Assert.True(XNode.DeepEquals(exTree, acTree));
+
+
+            }
+        [Fact]
+        public void Test2()
+        {
+            var exTree = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<OverLimStat>
+  <Passengers>
+    <Passenger>Aerbova</Passenger>
+    <OverspentDay Date=""2026-06-02"" DayTicketCount=""2"" SpentMoney=""350"" />
+    <OverspentDay Date=""2026-06-06"" DayTicketCount=""2"" SpentMoney=""450"" />
+  </Passengers>
+  <Passengers>
+    <Passenger>Berva</Passenger>
+    <OverspentDay Date=""2026-06-02"" DayTicketCount=""1"" SpentMoney=""140"" />
+  </Passengers>
+  <Passengers>
+    <Passenger>Smith</Passenger>
+    <OverspentDay Date=""2026-06-02"" DayTicketCount=""1"" SpentMoney=""400"" />
+    <OverspentDay Date=""2026-06-06"" DayTicketCount=""1"" SpentMoney=""525"" />
+  </Passengers>
+</OverLimStat>");
+            var acTree = Transport2Exam.TransportLogic.OverLimitCommuting(_fixture.Passengers, _fixture.Routes, _fixture.Infos);
+            Assert.True(XNode.DeepEquals(exTree, acTree));
+
+
+        }
+    }
+}
